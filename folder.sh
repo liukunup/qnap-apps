74d98ff03c05fe3c2de336823c33ed60f8197b52
Liu Kun <liukunup@163.com>
2022-10-30 16:59:33 +0800

    新增路径创建脚本

---
+++ folder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 待创建的路径列表
+folders=(
+  "/share/Container/grafana"
+  "/share/Container/code-server"
+  "/share/Container/code-server/config"
+  "/share/Container/minimal-notebook"
+)
+
+# 逐条检查 如路径不存在则创建
+for item in ${folders[*]}
+do
+  if [ ! -d "${item}" ];then
+    echo "create folder ${item} ..."
+    mkdir "${item}"
+  fi
+done
